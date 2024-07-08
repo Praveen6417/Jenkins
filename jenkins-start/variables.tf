26e5ae7bc67d720df9208e019c59ad1df5bc132c
Praveen6417 <praveenkasagalla123@gmail.com>
2024-07-08 18:09:27 +0530

    This is a Jenkins start File

---
+++ jenkins-start/variables.tf
@@ -0,0 +1,30 @@
+variable "cidr_blocks" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
+
+variable ingress_port_no{
+    type = number
+    default = 22
+}
+
+variable instance_type{
+    type = string
+    default = "t2.micro"
+}
+
+variable ami_id{
+    type = string
+    default = "ami-090252cbe067a9e58"
+}
+
+variable "instance_names" {
+    type = list(string)
+    default = ["Jenkins-Master", "Jenkins-Node"]
+}
+
+variable "common_tags" {
+    default = {
+        terraform = true
+    }
+}
