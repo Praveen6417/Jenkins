176658a429e32effae16542e816e24e81b064073
Praveen6417 <praveenkasagalla123@gmail.com>
2024-07-09 08:11:30 +0530

    This is a Jenkins start File

--- jenkins-start/main.tf
+++ jenkins-start/main.tf
@@ -10,7 +10,26 @@ resource "aws_instance" "sample" {
         Name = var.instance_names[count.index]
       }
   )
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"  # Replace with the username that has sudo access
+    host = self.public_ip
+  }
+
+  provisioner "file" {
+    source = "jenkins.repo"
+    destination = "/home/ec2-user/jenkins.repo"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo", "sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key", 
+    "sudo yum install fontconfig java-17-openjdk -y",
+    "sudo yum install jenkins -y", "sudo systemctl enable jenkins", 
+    "sudo systemctl start jenkins", "sudo systemctl daemon-reload" ]
+  }
 }
+ 
 
 resource "aws_security_group" "allow_all" {
     name= "allow_all"
