b4749279f8c917fd2daf0b388bff528b6597e3a5
Praveen6417 <praveenkasagalla123@gmail.com>
2024-07-10 23:37:04 +0530

    This is a Jenkins start File

---
+++ jenkins-start/output.tf
@@ -0,0 +1,15 @@
+output "public_ip_master" {
+  value = aws_instance.sample[0].public_io
+}
+
+output "public_ip_node" {
+  value = aws_instance.sample[1].public_ip
+}
+
+output "private_ip_master" {
+  value = aws_instance.sample[0].private_ip 
+}
+
+output "private_ip_node" {
+  value = aws_instance.sample[1].private_ip
+}
