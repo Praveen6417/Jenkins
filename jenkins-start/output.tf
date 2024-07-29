2157297dc9b7187721ee029ba7a2c19e908a9fdc
Praveen6417 <praveenkasagalla123@gmail.com>
2024-07-29 14:49:57 +0530

    This is a Jenkins Start file

--- jenkins-start/output.tf
+++ jenkins-start/output.tf
@@ -1,15 +1,15 @@
 output "public_ip_master" {
-  value = aws_instance.sample[0].public_ip
+  value = aws_instance.jenkins[0].public_ip
 }
 
 output "public_ip_node" {
-  value = aws_instance.sample[1].public_ip
+  value = aws_instance.jenkins[1].public_ip
 }
 
 output "private_ip_master" {
-  value = aws_instance.sample[0].private_ip 
+  value = aws_instance.jenkins[0].private_ip 
 }
 
 output "private_ip_node" {
-  value = aws_instance.sample[1].private_ip
+  value = aws_instance.jenkins[1].private_ip
 }
